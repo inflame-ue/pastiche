71cc16440adbd40d094c7c01a6b31d72633e77c8
Ilia Oberemok <i.oberemok777@gmail.com>
2026-06-30 18:35:42 +0300

    internal/formatter: implement the Formatter interface, and the GoFormatter that uses format.Source

---
+++ internal/formatter/gofmt.go
@@ -0,0 +1,15 @@
+package formatter
+
+import (
+	"go/format"
+)
+
+type GoFormatter struct{}
+
+func (gf *GoFormatter) Name() string {
+	return "go"
+}
+
+func (gf *GoFormatter) Format(src []byte) ([]byte, error) {
+	return format.Source(src)
+}
