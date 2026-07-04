853d1d66757b92028e4f2fe88dbdd1ecfa6133ba
Ilia Oberemok <i.oberemok777@gmail.com>
2026-07-04 10:34:49 +0300

    internal/trigger: implement the Autowatch functionality along with code detection hereustic

---
+++ internal/trigger/autowatch.go
@@ -0,0 +1,52 @@
+package trigger
+
+import (
+	"bytes"
+	"context"
+
+	"github.com/inflame-ue/pastiche/internal/pipeline"
+	"golang.design/x/clipboard"
+)
+
+func hereustic(src []byte, hereusticThreshold int) bool {
+	var hereusticHits int
+
+	if bytes.Count(src, []byte("\n")) >= 3 {
+		hereusticHits++
+	}
+
+	if bytes.ContainsAny(src, "{(=:;#") {
+		hereusticHits++
+	}
+
+	if bytes.Contains(src, []byte("==")) || bytes.Contains(src, []byte("!=")) {
+		hereusticHits++
+	}
+
+	if bytes.Contains(src, []byte("=>")) || bytes.Contains(src, []byte("->")) {
+		hereusticHits++
+	}
+
+	if bytes.Contains(src, []byte("//")) || bytes.Contains(src, []byte("/*")) {
+		hereusticHits++
+	}
+
+	return hereusticHits >= hereusticThreshold
+}
+
+func FormatAutowatch(ctx context.Context, p *pipeline.Pipeline, hereusticThreshold int) {
+	var lastSeen []byte
+	for data := range clipboard.Watch(ctx, clipboard.FmtText) {
+		src := data.Bytes
+
+		// this is at most a two cycle, because we write back to the clipboard
+		if bytes.Equal(src, lastSeen) {
+			continue
+		}
+
+		lastSeen = src
+		if hereustic(src, hereusticThreshold) {
+			p.Submit(src)
+		}
+	}
+}
