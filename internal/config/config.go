d84e0c4c791fad7e071c22ceb51cdfc74a0080ef
Ilia Oberemok <i.oberemok777@gmail.com>
2026-07-04 12:29:18 +0300

    internal/config: can now write Config struct to the config file

--- internal/config/config.go
+++ internal/config/config.go
@@ -35,6 +35,56 @@ func getConfigPath() (string, error) {
 	return filepath.Join(home, configFilePath), nil
 }
 
+func NewDefaultConfig() *Config {
+	return &Config{
+		Trigger: struct {
+			Mode string "toml:\"mode\""
+		}{
+			Mode: "autowatch",
+		},
+		Heuristic: struct {
+			Value int "toml:\"value\""
+		}{
+			Value: 3,
+		},
+		Formatters: struct {
+			Order []string "toml:\"order\""
+		}{
+			Order: []string{"go", "python", "rust"},
+		},
+	}
+}
+
+func Save(conf *Config) error {
+	confPath, err := getConfigPath()
+	if err != nil {
+		return fmt.Errorf("getting config path: %w", err)
+	}
+
+	err = os.MkdirAll(filepath.Dir(confPath), 0600)
+	if err != nil {
+		return fmt.Errorf("creating conf directory: %w", err)
+	}
+
+	file, err := os.Create(filepath.Base(confPath))
+	if err != nil {
+		return fmt.Errorf("opening conf file: %w", err)
+	}
+	defer file.Close()
+
+	data, err := toml.Marshal(conf)
+	if err != nil {
+		return fmt.Errorf("marshalling conf to bytes: %w", err)
+	}
+
+	_, err = file.Write(data)
+	if err != nil {
+		return fmt.Errorf("writing conf to file: %w", err)
+	}
+
+	return nil
+}
+
 func Load() (*Config, error) {
 	confPath, err := getConfigPath()
 	if err != nil {
